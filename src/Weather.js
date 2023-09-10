4f29cd555944f0ae8777296ac1b5b08c1edd46e9
Lidia GDC <lidiagdcprogramacion@gmail.com>
2023-09-10 16:30:16 +0200

    Create the html structure in Weather.js
    Copy the structure of Google weather

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,33 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <p> This is the weather component</p>
+      <form>
+        <input type="search" placeholder="Enter a city name..." />
+        <input type="submit" value="Search" />
+      </form>
+
+      <p> Results for Madrid</p>
+      <div className="row">
+        <div className="col-2">
+          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
+          28 °C
+        </div>
+        <div className="col-2">
+          <ul>
+            <li> Precipitation: 8% </li>
+            <li> Humidity: 50% </li>
+            <li> Wind: 8 Km/h </li>
+          </ul>
+        </div>
+
+        <div className="col-8">
+          <ul>
+            <li>Weather</li>
+            <li>Sunday, 16:00</li>
+            <li> Partly cloudy</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
