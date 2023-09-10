4f629ecfcdb64b1794969fb9de4d6888b81f40bd
Lidia GDC <lidiagdcprogramacion@gmail.com>
2023-09-10 13:18:53 +0200

    Added link to github repository in App.js
    Add a link inside a footer in the app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <footer>
+        This was coded by Lidia Gallardo and is
+        <a
+          href="https://github.com/LidiaGDC/wfiveweather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-source{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
