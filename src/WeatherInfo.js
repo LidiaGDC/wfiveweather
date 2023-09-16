fd6eee71f376a335d057148f8f8ee95f87085f5c
Lidia GDC <lidiagdcprogramacion@gmail.com>
2023-09-16 18:51:53 +0200

    Create unit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import CorrectedDate from "./CorrectedDate";
 import WeatherIcon from "./WeatherIcon";
 import "./Weather.css";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -11,23 +12,22 @@ export default function WeatherInfo(props) {
         Results for <span className="city-picked"> {props.data.city} </span>
       </h1>
       <div className="row">
-        <div className="col-2 ">
+        <div className="col-sm-2 ">
           <div className="float-left icon">
             <WeatherIcon code={props.data.icon} />
           </div>
         </div>
-        <div className="col-2 current-temp">
-          <span className="number">{props.data.temperature}</span>
-          <span className="unit">°C</span>
+        <div className="col-sm-3 current-temp">
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
-        <div className="col-3 weather-data">
+        <div className="col-sm-3 weather-data">
           <ul>
             <li> Humidity: {props.data.humidity} % </li>
             <li> Wind: {props.data.wind} Km/h </li>
           </ul>
         </div>
 
-        <div className="col-5 date-info">
+        <div className="col-sm-4 date-info">
           <ul>
             <li className="right-title"> Weather </li>
             <li className="weekday ">
---
+++ src/WeatherTemperature.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return Math.round((props.celsius * 9) / 5 + 32);
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="weatherTemperatureCelsius">
+        {" "}
+        <span className="number">{props.celsius}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit} className="text-decoration-none">
+            °F{" "}
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="weatherTemperatureFahrenheit">
+        {" "}
+        <span className="number">{fahrenheit()}</span>
+        <span className="unit">
+          <a href="/" onClick={showCelsius} className="text-decoration-none">
+            {" "}
+            °C{" "}
+          </a>
+          | °F
+        </span>
+      </div>
+    );
+  }
+}
