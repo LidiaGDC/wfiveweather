0c34048a89ab3eb1cc3078ddcb97a7c75820b9da
Lidia GDC <lidiagdcprogramacion@gmail.com>
2023-09-22 19:45:31 +0200

    Conect Api call with the layout

---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,33 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+  return (
+    <div className="weatherForecastDay">
+      <div className="WeatherForecast-day">{props.data.dt}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={35} />
+      <div className="WeatherForecast-temp">
+        <span className="WeatherForecast-tempmax"> {maxTemperature()}°</span>
+        <span className="WeatherForecast-tempmin">{minTemperature()}°</span>
+      </div>
+    </div>
+  );
+}
